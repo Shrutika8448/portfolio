b038e6a776059050113140c24ca57703a12b117b
Shrutika <164064554+Shrutika8448@users.noreply.github.com>
2025-11-29 08:49:07 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,23 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', // THIS IS CRITICAL
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        olive: {
+          dark: "#3F4E1F",
+          DEFAULT: "#6B7A2F",
+          soft: "#8C9B40",
+          accent: "#B7C667",
+        },
+        offwhite: {
+          light: "#F7F4EB",
+          DEFAULT: "#EEE8DA",
+          deep: "#DDD2BD",
+        },
+      },
+    },
   },
   plugins: [],
 }
